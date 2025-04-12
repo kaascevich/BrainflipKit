e96c20ceaa2c298464cb91d4b62d1f300fccb1dc
Kaleb A. Ascevich <kaascevich@noreply.codeberg.org>
2025-04-12 16:58:46 -0400

    chore: 📄 fix wrong project being referenced in license comments

--- Sources/BrainflipKit/Interpreter/Options.swift
+++ Sources/BrainflipKit/Interpreter/Options.swift
@@ -1,17 +1,17 @@
 // This file is part of BrainflipKit.
 // Copyright © 2024-2025 Kaleb A. Ascevich
 //
-// Haven is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Affero General Public License (GNU AGPL) as published by the
-// Free Software Foundation, either version 3 of the License, or (at your
+// BrainflipKit is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License (GNU AGPL) as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
 //
-// Haven is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-// A PARTICULAR PURPOSE. See the GNU AGPL for more details.
+// BrainflipKit is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU AGPL for more details.
 //
-// You should have received a copy of the GNU AGPL along with Haven. If not, see
-// <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU AGPL along with BrainflipKit. If
+// not, see <https://www.gnu.org/licenses/>.
 
 public extension Interpreter {
   /// Configurable options for an ``Interpreter`` instance.
